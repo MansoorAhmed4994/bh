63c8a79c825fc78d63c3477c5cb5d96a3e1aca78
MansoorAhmed4994 <68462121+MansoorAhmed4994@users.noreply.github.com>
2021-10-20 05:23:14 +0500

    Create Users.php

---
+++ app/Models/Users.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class Users extends Model
+{
+    use Notifiable;
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name','last_name', 'email', 'password','phone','address','role_id',
+    ];
+
+    /** 
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany('App\Models\Role');
+    }
+    //
+}
