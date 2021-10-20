e8164a68e3175ccf7c06ea5fdd6ba04aa490dac3
MansoorAhmed4994 <68462121+MansoorAhmed4994@users.noreply.github.com>
2021-10-20 05:21:11 +0500

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,7 +8,7 @@ class Role extends Model
 {
     public function users()
     {
-        return $this->belongsToMany('App\User');
+        return $this->belongsToMany('App\Models\User');
     }
     //
 }
