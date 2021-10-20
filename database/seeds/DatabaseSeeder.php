92b033bb375f8dd56ceae2b609d8761d88519b63
MansoorAhmed4994 <68462121+MansoorAhmed4994@users.noreply.github.com>
2021-10-20 05:21:43 +0500

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserRolesTableSeeder::class,
+            
+            RolesTableSeeder::class,
             UsersTableSeeder::class,
         ]);
         
