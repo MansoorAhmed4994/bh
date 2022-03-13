60cca77bd01280e1b31b8d91a2b188fefc57e4fc
MansoorAhmed4994 <mansoor.zaheer994@gmail.com>
2022-03-13 18:02:23 -0400

    changes for list page

--- resources/views/client/orders/manual-orders/list.blade.php
+++ resources/views/client/orders/manual-orders/list.blade.php
@@ -4,6 +4,7 @@
 <script>
 var base_url = '<?php echo e(url('/')); ?>';
 var dispatch_order_id =  '';
+var order_status = '';
     $( document ).ready(function() { 
                 $('.pop').on('click', function() {
                     // alert($(this).attr('src'));
@@ -13,7 +14,7 @@ var dispatch_order_id =  '';
                 
                 
             
-        $('#save_dispatch_order_edit').on('click',function(){
+        $('#save_order_status_and_price').on('click',function(){
         
     
         let name = $('#receiver_name').val();
@@ -33,6 +34,7 @@ var dispatch_order_id =  '';
             number:number,
             address:address,
             price:price,
+            status:order_status,
           },
           success:function(response){
             //$('#successMsg').show();
@@ -55,9 +57,11 @@ var dispatch_order_id =  '';
     });
     
     
-    function dispatch_order(val) 
+    function change_order_status_and_price(val,status) 
     {  
+        alert('working');
         dispatch_order_id = val;
+        order_status = status;
         //alert('working');
         $.ajax({
             headers: {
@@ -84,6 +88,7 @@ var dispatch_order_id =  '';
             }
         });
     }
+    
     
     function get_checked_values()
     {
@@ -176,11 +181,12 @@ var dispatch_order_id =  '';
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" id="save_dispatch_order_edit">Save changes</button>
+                <button type="button" class="btn btn-primary" id="save_order_status_and_price">Save changes</button>
             </div>
         </div>
     </div>
 </div>
+
 
 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
@@ -264,7 +270,7 @@ var dispatch_order_id =  '';
 </nav>
 
 <div style="overflow-x:auto;"> 
-    <table class="table table-bordered">
+    <table class="table table-bordered" style="min-height: 500px;">
         <thead>
             <tr> 
                 <th scope="col"></th>
@@ -276,21 +282,21 @@ var dispatch_order_id =  '';
                 <!--<th scope="col">Confirmed</th>-->
                 <!--<th scope="col">Dispatched</th>-->
                 <!--<th scope="col">deleted</th>-->
-                <th scope="col">Actions</th>
-                <th scope="col">Order ID</th>
-                <th scope="col">First Name</th> 
-                <th scope="col">Reciever Number</th>
+                <th scope="col">Act</th>
+                <th scope="col">Ord. ID</th>
+                <th scope="col">F. Name</th> 
+                <th scope="col">Rec. Number</th>
                 <th scope="col">Number</th>
                 <th scope="col">Description</th>
                 <!--<th scope="col">Ord. Location</th>-->
                 <th scope="col">Address</th>
                 <th scope="col">Price</th>
-                <th scope="col">Images</th>
-                <th scope="col">Total Pieces</th>
-                <th scope="col">Ord.paid Date</th>
-                <th scope="col">created Date</th>
-                <th scope="col">Updated Date</th>
-                <th scope="col">Ord.status</th>
+                <th scope="col">Img.</th>
+                <!--<th scope="col">Total Pieces</th>-->
+                <!--<th scope="col">Ord.paid Date</th>-->
+                <th scope="col">cr. Date</th>
+                <th scope="col">Up. Date</th>
+                <th scope="col">status</th>
             </tr>
         </thead>
         <tbody>  
@@ -327,10 +333,10 @@ var dispatch_order_id =  '';
                             <a type="button" target="_blank" href="{{route('ManualOrders.edit',$lists->id)}}" class="dropdown-item">Edit</a>             
                             <a type="button" target="_blank" href="{{route('ManualOrders.show',$lists->id)}}" class="dropdown-item">view</a>
                             <a type="button" href="{{route('ManualOrders.print.order.slip',$lists->id)}}" class="dropdown-item">Print Slip</a>
-                            <a type="button" href="{{route('ManualOrders.order.status',['prepared',$lists->id])}}" class="dropdown-item">Prepare</a> 
-                            <a type="button" href="{{route('ManualOrders.order.status',['confirmed',$lists->id])}}" class="dropdown-item">Confirmed</a> 
+                            <a type="button" href="{{route('ManualOrders.order.status',['prepared',$lists->id])}}" class="dropdown-item">Prepare</a>   
                             <a type="button" href="{{route('ManualOrders.order.status',['complete',$lists->id])}}" class="dropdown-item">Complete</a> 
-                            <button type="button" id="dispatch-btn" onclick="dispatch_order({{$lists->id}})" class="dropdown-item" data-toggle="modal" data-target="#exampleModalCenter">Dispatch</button>
+                            <button type="button" id="dispatch-btn" onclick="change_order_status_and_price({{$lists->id}},'confirmed')" class="dropdown-item" data-toggle="modal" data-target="#exampleModalCenter">Confirmed</button>
+                            <button type="button" id="dispatch-btn" onclick="change_order_status_and_price({{$lists->id}},'dispatched')" class="dropdown-item" data-toggle="modal" data-target="#exampleModalCenter">Dispatch</button>
                             <a type="button" href="{{route('ManualOrders.order.status',['hold',$lists->id])}}" class="dropdown-item">Hold</a> 
                             <a type="button" href="{{route('ManualOrders.order.status',['incomplete',$lists->id])}}" class="dropdown-item">Incomplete</a>
                             <a type="button" href="{{route('ManualOrders.order.status',['cancel',$lists->id])}}" class="dropdown-item">Cancel</a>
@@ -361,12 +367,12 @@ var dispatch_order_id =  '';
                 <th >
                     @if(!empty($lists->images))
                         @foreach(explode('|', $lists->images) as $image)   
-                        <img class="pop rounded " style="margin-right: 5px;" src="{{asset($image)}}" alt="Card image cap" width="50" height="50">
+                        <img class="pop rounded " style="margin-right: 5px;" src="{{asset($image)}}" alt="Card image cap" width="25" height="25">
                         @endforeach
                     @endif
                 </th>
-                <th>{{$lists->total_pieces}}</th>
-                <th>{{$lists->date_order_paid}}</th>
+                <!--<th>{{$lists->total_pieces}}</th>-->
+                <!--<th>{{$lists->date_order_paid}}</th>-->
                 <th>{{$lists->created_at}}</th>
                 <th>{{$lists->updated_at}}</th>
                 <th>{{$lists->status}}</th>
