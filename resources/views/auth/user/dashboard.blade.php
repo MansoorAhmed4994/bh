88af57e52be6e3b5dd63ee2ab380c58dc91eda7a
MansoorAhmed4994 <mansoor.zaheer994@gmail.com>
2024-09-23 23:06:38 -0400

    chan

--- resources/views/auth/user/dashboard.blade.php
+++ resources/views/auth/user/dashboard.blade.php
@@ -5,26 +5,26 @@
 <div class="container">
     <div class="row">  
     <?php $total_orders=0;?>
-        @foreach($data as $list)
-        <div class="col-sm-4 form-group">  
-                <div class="card" style="width: 100%;"> 
-                  <div class="card-body status-{!! str_replace(' ', '-', $list->status) !!} dashbord-card-body">
-                    <h3>{{$list->status}}</h3>
-                    <h5 class="card-title">{{$list->total_orders}}</h5> 
-                    <h5 class="card-title">{{$list->total_amount}}</h5>  
-                    <br>
-                    <form action="{{route('ManualOrders.index')}}" method="post">
-                        @csrf
-                        <input type="hidden" name="order_status"  value="{{$list->status}}">
-                    <button type="submit" class="btn btn-primary">Go</button>
-                    </form>
+        <!--@foreach($data as $list)-->
+        <!--<div class="col-sm-4 form-group">  -->
+        <!--        <div class="card" style="width: 100%;"> -->
+        <!--          <div class="card-body status-{!! str_replace(' ', '-', $list->status) !!} dashbord-card-body">-->
+        <!--            <h3>{{$list->status}}</h3>-->
+        <!--            <h5 class="card-title">{{$list->total_orders}}</h5> -->
+        <!--            <h5 class="card-title">{{$list->total_amount}}</h5>  -->
+        <!--            <br>-->
+        <!--            <form action="{{route('ManualOrders.index')}}" method="post">-->
+        <!--                @csrf-->
+        <!--                <input type="hidden" name="order_status"  value="{{$list->status}}">-->
+        <!--            <button type="submit" class="btn btn-primary">Go</button>-->
+        <!--            </form>-->
                     <!--<a href="{{route('ManualOrders.status.order.list',$list->status)}}" class="btn btn-primary">Go</a>-->
-                  </div> 
-                </div>
-            </div>
+        <!--          </div> -->
+        <!--        </div>-->
+        <!--    </div>-->
             
-            <?php $total_orders += $list->total; ?>
-        @endforeach 
+        <!--    <?php $total_orders += $list->total; ?>-->
+        <!--@endforeach -->
         
         <div class="col-sm-4 form-group">  
             <div class="card" style="width: 18rem;"> 
